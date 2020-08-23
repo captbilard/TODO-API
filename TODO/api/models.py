d6fed99791049ecf6a30bf63318a53f5ef9a16b9
captbilard <bilardosinulu@gmail.com>
2020-08-23 14:05:24 +0100

    Setup default configuration for the API

---
+++ TODO/api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=500)
+    completed = models.BooleanField()
+    url = models.CharField(max_length=500)
+    order = models.IntegerField()
+
+    def __str__(self):
+        self.title
